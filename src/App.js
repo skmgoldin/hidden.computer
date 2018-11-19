edf5eb6e7d23457699a06907440026e8a17b73d7
Mike Goldin <mike.goldin@consensys.net>
2018-11-18 19:01:22 -0500

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,30 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+import React, { Component } from 'react';
+import { HashRouter as Router, Route, Switch } from 'react-router-dom';
+
+import Header from './Header.js';
+import About from './About.js';
+import Papers from './Papers.js';
+import Code from './Code.js';
+
+const Home = () => (<div />);
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <Router >
+        <div>
+          <Header />
+          <Switch>
+            <Route path="/" component={Home} exact />
+            <Route path="/about" component={About} />
+            <Route path="/papers" component={Papers} />
+            <Route path="/code" component={Code} />
+            <Route />
+          </Switch>
+        </div>
+      </Router>
     );
   }
 }
