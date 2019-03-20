83056c4c747eff863be867a617900b04867494bb
Mike Goldin <mike.goldin@protonmail.ch>
2019-03-20 14:31:43 -0400

    Add exponential backoff to stack deletion queries

--- scripts/deleteWww.sh
+++ scripts/deleteWww.sh
@@ -17,13 +17,17 @@ aws s3 rm s3://$StaticOutputBucket/ \
   --recursive
 
 echo deleting associated service stack $SERVICE_STACK_NAME
+let waitTime=1
 aws cloudformation delete-stack \
   --stack-name $SERVICE_STACK_NAME
 while [ $? -eq 0 ]
 do
-  sleep 5
+  echo $(date +%H:%M:%S): Backing off to wait $waitTime seconds before querying service stack \
+    deletion again...
+  sleep $waitTime
+  let waitTime=$(( ($waitTime+($RANDOM%5))*2 ))
   aws cloudformation describe-stacks \
-    --stack-name $SERVICE_STACK_NAME
+    --stack-name $SERVICE_STACK_NAME >/dev/null 2>&1
 done
 
 echo deleting build stack $BUILD_STACK_NAME
