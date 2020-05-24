482013390f620fe0bcb0572e1e8d54dbfefd0cbb
Mike Goldin <mike.goldin@protonmail.ch>
2020-05-23 22:24:26 -0400

    Content update

--- src/About.js
+++ src/About.js
@@ -7,24 +7,12 @@ class About extends Component {
     return (
       <div className="about">
         <img src={Me} alt="" />
-        <p>
-          At Coney Island with{" "}
-          <a href="https://www.instagram.com/joann.arosemena/">Jo-Ann</a>
-        </p>
         <h3>Me</h3>
         <div>
           <p>
             I am a software developer at{" "}
             <a href="http://consensys.net">ConsenSys</a> in Brooklyn, where I
-            direct the token-curated registries team. I spend most of my time in
-            the EVM and write a little Javascript, too. I like data structures.
-            I have a{" "}
-            <a href="https://docs.google.com/document/d/1TcceAsBlAoFLWSQWYyhjmTsZCp0XqRhNdGMb6JbASxc/edit?usp=sharing">
-              cryptosystems manifesto
-            </a>
-            . My interest is in fully decentralized cryptosystems, and in using
-            incentive games to program humans. I went to Bennington College for
-            a while and graduated from Columbia University with a CS degree.
+            currently work on the{" "} <a href="http://infura.io">Infura</a> team.
           </p>
           <p>My email address is mike dot goldin at consensys.net</p>
         </div>
